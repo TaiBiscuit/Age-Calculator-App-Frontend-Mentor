9b358f4d0d089378b79e35bdd1ddd5de0722e2c8
nicolc <nicolas.l.campos1@gmail.com>
2023-05-10 21:22:10 -0300

    Styling Input and Js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const yearF = document.getElementById('yearF');
 const outputDay = document.getElementById('output-day');
 const outputMonth = document.getElementById('output-month');
 const outputYear = document.getElementById('output-year');
+
+const inputDay = document.getElementsByClassName('dayF');
+const inputMonth = document.getElementsByClassName('monthF');
+const inputYear = document.getElementsByClassName('yearF');
+const outputs = document.querySelectorAll('.inpQue');
+
+
 const DATE = new Date;
 let day = DATE.getDate();
 let month = DATE.getMonth() + 1;
@@ -17,17 +24,29 @@ form.addEventListener('submit', (e) => {
     const inputMonthValue = monthF.value;
     const inputYearValue = yearF.value;
     const dayExisted = monthList[inputMonthValue - 1] - inputDayValue ;
+
+    for(let i = 0; i < outputs.length; i++){
+        if(outputs[i].value.length == 0){
+            console.log(outputs[i].value.length)
+            outputs[i].classList.remove('unfilled');
+        } else {
+            const birthDate = new Date(`${inputMonthValue}/${inputDayValue}/${inputYearValue}`);
+            const birthDay = birthDate.getDate();
+            const birthMonth = birthDate.getMonth() +1;
+            const birthYear = birthDate.getFullYear();
+            calculateDif(birthDay, birthMonth, birthYear); 
+        }
+    } 
     if(inputYearValue > year){
-        return alert('We are not in the future, buddy!');
+        inputYear[0].classList.remove('unfilled');
     }
+
     if(dayExisted < 0) {
-        return alert(`That day didn't exist, babe`);
-    }  
-    const birthDate = new Date(`${inputMonthValue}/${inputDayValue}/${inputYearValue}`);
-    const birthDay = birthDate.getDate();
-    const birthMonth = birthDate.getMonth() +1;
-    const birthYear = birthDate.getFullYear();
-    calculateDif(birthDay, birthMonth, birthYear); 
+        const hasIt = dayF.classList.contains('unfilled');
+        inputDay[0].classList.remove('unfilled') 
+        console.log(inputDay[0].classList)
+        return console.log('nope')
+    } 
 });
 
 const cleanForm = () => {
@@ -37,19 +56,26 @@ const cleanForm = () => {
 };
 
 const calculateDif = (dayF, monthF, yearF) => {
-    let resultDay = dayF - day;
+    let resultDay = day;
     let resultYear = (yearF - year) * -1; 
-    countUp(resultDay, outputDay);
-    if((monthF - month) <= 0) {
-        const resultMonth = 10;
-        console.log(resultMonth)
+    let resultMonth = 0;
+    if((monthF - month) < 0) {
+        resultMonth = (monthF - month) * -1;
+        countUp(resultDay, outputDay);
         countUp(resultMonth, outputMonth);
-        resultYear++;
+        countUp(resultYear, outputYear); 
+    } else if((monthF - month == 0)){
+        console.log(dayF)
+        resultDay = dayF;
+        resultMonth = 1;
+        countUp(resultDay, outputDay);
+        countUp(resultMonth, outputMonth);
         countUp(resultYear, outputYear); 
     } else {
-        const resultMonth = monthF - month;
+        resultMonth = (12 - monthF + month);
+        resultYear--;
+        countUp(resultDay, outputDay);
         countUp(resultMonth, outputMonth);
-        const resultYear = (yearF - year + 1) * -1; 
         countUp(resultYear, outputYear); 
     }
 };
